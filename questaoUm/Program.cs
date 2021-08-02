837a6c26cd60a9da51ca59b8d1d1b5b945f17da1
Leonardo A. Pregnolato <78579109+leopregnolato@users.noreply.github.com>
2021-08-02 17:55:14 -0300

    Add files via upload

--- questaoUm/Program.cs
+++ questaoUm/Program.cs
@@ -6,7 +6,7 @@ namespace questaoUm
 {
     class Program
     {
-        static void Main(string[] args, double[] ranking)
+        static void Main(string[] args)
         {
             List<Atleta> atletas = new List<Atleta>();
             atletas.Add(new Atleta("x","x", 0));
@@ -37,26 +37,72 @@ namespace questaoUm
             Console.WriteLine("Digite o RESULTADO (em metros) do atleta 2: ");
             atletas[1].Distancia = Convert.ToDouble(Console.ReadLine());
 
-            double[] posicoes = new double[2];
-            
-            for(int i=0; i < atletas.Count; i++)
+            Console.WriteLine("Digite o NOME do atleta 3: ");
+            atletas[2].Nome = Console.ReadLine();
+            Console.WriteLine("Digite o PAÍS do atleta 3: ");
+            atletas[2].Pais = Console.ReadLine();
+            Console.WriteLine("Digite o RESULTADO (em metros) do atleta 3: ");
+            atletas[2].Distancia = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o NOME do atleta 4: ");
+            atletas[3].Nome = Console.ReadLine();
+            Console.WriteLine("Digite o PAÍS do atleta 4: ");
+            atletas[3].Pais = Console.ReadLine();
+            Console.WriteLine("Digite o RESULTADO (em metros) do atleta 4: ");
+            atletas[3].Distancia = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o NOME do atleta 5: ");
+            atletas[4].Nome = Console.ReadLine();
+            Console.WriteLine("Digite o PAÍS do atleta 5: ");
+            atletas[4].Pais = Console.ReadLine();
+            Console.WriteLine("Digite o RESULTADO (em metros) do atleta 5: ");
+            atletas[4].Distancia = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o NOME do atleta 6: ");
+            atletas[5].Nome = Console.ReadLine();
+            Console.WriteLine("Digite o PAÍS do atleta 6: ");
+            atletas[5].Pais = Console.ReadLine();
+            Console.WriteLine("Digite o RESULTADO (em metros) do atleta 6: ");
+            atletas[5].Distancia = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o NOME do atleta 7: ");
+            atletas[6].Nome = Console.ReadLine();
+            Console.WriteLine("Digite o PAÍS do atleta 7: ");
+            atletas[6].Pais = Console.ReadLine();
+            Console.WriteLine("Digite o RESULTADO (em metros) do atleta 7: ");
+            atletas[6].Distancia = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o NOME do atleta 8: ");
+            atletas[7].Nome = Console.ReadLine();
+            Console.WriteLine("Digite o PAÍS do atleta 8: ");
+            atletas[7].Pais = Console.ReadLine();
+            Console.WriteLine("Digite o RESULTADO (em metros) do atleta 8: ");
+            atletas[7].Distancia = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o NOME do atleta 9: ");
+            atletas[8].Nome = Console.ReadLine();
+            Console.WriteLine("Digite o PAÍS do atleta 9: ");
+            atletas[8].Pais = Console.ReadLine();
+            Console.WriteLine("Digite o RESULTADO (em metros) do atleta 9: ");
+            atletas[8].Distancia = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o NOME do atleta 10: ");
+            atletas[9].Nome = Console.ReadLine();
+            Console.WriteLine("Digite o PAÍS do atleta 10: ");
+            atletas[9].Pais = Console.ReadLine();
+            Console.WriteLine("Digite o RESULTADO (em metros) do atleta 10: ");
+            atletas[9].Distancia = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("Ordenação das posições:");
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            var podium = atletas.OrderBy(atletas => atletas.Distancia);           
+            foreach(var a in podium)
             {
-                posicoes[i] = atletas[i].Distancia;
-            }
-
-            Array.Sort(posicoes); 
-
-            double primeiro = posicoes[0];
-            double segundo = posicoes[1];
-            double terceiro = posicoes[2];
-
-            // var podium = atletas.GroupBy(atletas => atletas.Distancia);    
-            // foreach(var group in atletas)
-            // {
-            //     if(primeiro = Distancia)
-                
-            //     Console.WriteLine(group.Key + ": " + group.Count());
-            // }     
-            
+            Console.WriteLine("{0} {1} {2}", a.Nome, a.Pais, a.Distancia);
+            } 
+        }   
     }
 }
